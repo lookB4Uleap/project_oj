6c73205381aacd8037403629468730e13a0fe3a5
Anish Sarkar <54957069+lookB4Uleap@users.noreply.github.com>
2024-09-22 19:59:41 +0530

    Revert "Added create problem feature"

--- backend_codolympics/src/routes/testcases.ts
+++ backend_codolympics/src/routes/testcases.ts
@@ -1,15 +1,10 @@
 import { NextFunction, Request, Response, Router } from "express";
 import Testcase, { TestcaseType } from "../models/testcases";
-import { authorize } from "../middlewares/authorize";
-import { verifyAdmin } from "../middlewares/verify-admin";
-import { v4 as uuid } from 'uuid';
-import 'dotenv/config';
-import { generatePresignedURL } from "../utils/file-upload";
 
 const router = Router();
 const TESTCASE_TYPE = "visible";
 
-export const saveTestcase = async (testcaseProp: TestcaseType) => {
+const saveTestcase = async (testcaseProp: TestcaseType) => {
     try {
         const testcase = new Testcase({
             problemId: testcaseProp.problemId,
@@ -25,17 +20,17 @@ export const saveTestcase = async (testcaseProp: TestcaseType) => {
     }
 }
 
-router.post('/', authorize, verifyAdmin, async (req: Request, res: Response, next: NextFunction) => {
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
     const problemId = req.body.problemId;
     const type = req.body.type ?? TESTCASE_TYPE;
     const points = req.body.points;
-    const input = req.body.input.split("?")[0];
-    const output = req.body.output.split("?")[0];
+    const input = req.body.input;
+    const output = req.body.input;
 
-    if (!problemId || !input || !output)
+    if (!problemId || !points || !input || !output)
         return res.status(400).json({ message: "Problem fields missing!" });
 
-    const { testcase, error } = await saveTestcase({
+    const {testcase, error} = await saveTestcase({
         problemId,
         type,
         points,
@@ -47,23 +42,6 @@ router.post('/', authorize, verifyAdmin, async (req: Request, res: Response, nex
         return next(error);
 
     res.status(201).json({ message: 'Testcase created successfully', testcase });
-});
-
-router.get('/upload', authorize, verifyAdmin, async (req: Request, res: Response, next: NextFunction) => {
-    const fileId = uuid();
-    const fileName = `${fileId}.txt`;
-    const bucketName = process.env.AWS_S3_BUCKET;
-    if (!bucketName) {
-        console.log('[backend-codolympics] Bucket name is empty', bucketName);
-        return res.status(500).json({ message: "Internal server error!" });
-    }
-
-    const { url, error } = await generatePresignedURL(bucketName, fileName);
-
-    if (error)
-        return next(error);
-
-    res.status(200).json({ fileName, url });
 });
 
 export default router;
