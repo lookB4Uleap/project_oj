a14221ba166533126b25ad21891604b8816b05de
Anish <anish.sarkar.official@gmail.com>
2024-09-26 15:39:32 +0530

    Added get testcase route

--- backend_codolympics/src/routes/testcases.ts
+++ backend_codolympics/src/routes/testcases.ts
@@ -5,6 +5,7 @@ import { verifyAdmin } from "../middlewares/verify-admin";
 import { v4 as uuid } from 'uuid';
 import 'dotenv/config';
 import { generatePresignedURL } from "../utils/file-upload";
+import axios from "axios";
 
 const router = Router();
 const TESTCASE_TYPE = "visible";
@@ -47,6 +48,48 @@ router.post('/', authorize, verifyAdmin, async (req: Request, res: Response, nex
         return next(error);
 
     res.status(201).json({ message: 'Testcase created successfully', testcase });
+});
+
+const getTestcase = async (testcaseId: string) => {
+    try {
+        const testcase = await Testcase.findById(testcaseId).lean();
+        return { testcase, error: null };
+    }
+    catch (error: any) {
+        return { testcase: null, error };
+    }
+}
+
+const getTestcaseDataFromUrl = async (url: string) => {
+    const {data} = await axios.get(url);
+    return data;
+}
+
+const getTestcaseData = async (input: string, output: string) => {
+    try {
+        const inputRequest = getTestcaseDataFromUrl(input);
+        const outputRequest = getTestcaseDataFromUrl(output);
+        const [inputData, outputData] = await Promise.all([inputRequest, outputRequest]);
+        return {inputData, outputData, error: null};
+    } catch (error: any) {
+        return {inputData: null, outputData: null, error};
+    }
+}
+
+router.get("/:id", authorize, async (req: Request, res: Response, next: NextFunction) => {
+    const testcaseId = req.params.id;
+    const { testcase, error: FetchTestcaseError } = await getTestcase(testcaseId);
+    if (FetchTestcaseError) 
+        return next(FetchTestcaseError);
+    if (!testcase?.input || !testcase?.output)
+        return res.status(400).json({message: "Invalid testcase!"});
+
+    const {inputData, outputData, error: LoadTestcaseError} = await getTestcaseData(testcase?.input, testcase?.output);
+    if (LoadTestcaseError)
+        return next(LoadTestcaseError);
+
+    res.status(200).json({input: inputData, output: outputData});
+
 });
 
 router.get('/upload', authorize, verifyAdmin, async (req: Request, res: Response, next: NextFunction) => {
